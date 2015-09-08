f49165abe5b61c0fef7ae3171713f9124806dbce
wathika <vcwathika@gmail.com>
2015-09-09 00:28:21 +0100

    fixed analytics

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-57120854-1', 'auto');
-ga('require', 'displayfeatures');
+ga('create', 'UA-65072222-2', 'auto');
 ga('send', 'pageview');
+
 
 $( document ).ready(function() {
   $("[rel='tooltip']").tooltip();
